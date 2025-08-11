bdd1dad1c5c1051c7078558e6b169125e94787a8
Diego Carlos <diegoc.gayoso@gmail.com>
2025-08-10 22:40:40 -0300

    feat: add auth with Google Provider

--- src/components/common/category-list.tsx
+++ src/components/common/category-list.tsx
@@ -11,13 +11,13 @@ const CategoryList = () => {
   return (
     <>
       <div className="m-4 rounded-2xl bg-[#F4EFFF] p-4">
-        <ul className="flex flex-wrap gap-2 justify-center">
+        <div className="flex flex-wrap gap-2 justify-center">
           {categories.map((category) => (
-            <li key={category} className="text-xs text-center font-semibold bg-white py-2 px-4 max-h-10  max-w-[148.5px] rounded-2xl flex-1/2 flex items-center justify-center">
+            <button key={category} className="text-xs text-center font-semibold bg-white py-2 px-4 max-h-10  max-w-[148.5px] rounded-2xl flex-1/2 flex items-center justify-center">
               {category}
-            </li>
+            </button>
           ))}
-        </ul>   
+        </div>   
       </div>
     </>
   );
