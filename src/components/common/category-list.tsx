32b7021e6584087106b15c16d4f9978e8c189a09
Diego Carlos <diegoc.gayoso@gmail.com>
2025-08-12 10:14:40 -0300

    fix: added category query to the database

--- src/components/common/category-list.tsx
+++ src/components/common/category-list.tsx
@@ -1,20 +1,15 @@
-const CategoryList = () => {
-  const categories = [
-    "T-shirts",
-    "Pants",
-    "Shoes",
-    "Accessories",
-    "Shorts",
-    "Jackets and sweatshirts",
-  ];
+import { db } from "@/db";
+
+const CategoryList = async () => {
+  const categories = await db.query.categoryTable.findMany();
 
   return (
     <>
       <div className="m-4 rounded-2xl bg-[#F4EFFF] p-4">
         <div className="flex flex-wrap gap-2 justify-center">
           {categories.map((category) => (
-            <button key={category} className="text-xs text-center font-semibold bg-white py-2 px-4 max-h-10  max-w-[148.5px] rounded-2xl flex-1/2 flex items-center justify-center">
-              {category}
+            <button key={category.id} className="text-xs text-center font-semibold bg-white py-2 px-4 max-h-10  max-w-[148.5px] rounded-2xl flex-1/2 flex items-center justify-center">
+              {category.name}
             </button>
           ))}
         </div>   
