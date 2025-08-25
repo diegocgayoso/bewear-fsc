c22a4d35a62da2d4f13f05e42935d6d4b49b27cc
Diego Carlos <diegoc.gayoso@gmail.com>
2025-08-25 14:40:08 -0300

    fix. product variant fix

--- src/app/product-variant/[slug]/page.tsx
+++ src/app/product-variant/[slug]/page.tsx
@@ -9,10 +9,7 @@ import formatCentsToBrl from "@/helpers/money";
 import { Button } from "@/components/ui/button";
 import ProductsList from "@/components/common/products-list";
 import Footer from "@/components/common/footer";
-<<<<<<< HEAD
 import VariantSelector from "@/app/product/[slug]/components/variant-selector";
-=======
->>>>>>> c7183bf43307b805b7149197807688e64cd22dc5
 
 interface ProductVariantPageProps {
   params: Promise<{ slug: string }>;
@@ -23,15 +20,11 @@ const ProductPage = async ({ params }: ProductVariantPageProps) => {
   const productVariant = await db.query.productVariantTable.findFirst({
     where: eq(productVariantTable.slug, slug),
     with: {
-<<<<<<< HEAD
       product: {
         with: {
           variants: true,
         },
       },
-=======
-      product: true,
->>>>>>> c7183bf43307b805b7149197807688e64cd22dc5
     },
   });
   if (!productVariant) return notFound();
@@ -40,7 +33,6 @@ const ProductPage = async ({ params }: ProductVariantPageProps) => {
     where: eq(productTable.categoryId, productVariant.product.categoryId),
     with: {
       variants: true,
-<<<<<<< HEAD
       category: true,
     },
   });
@@ -51,14 +43,6 @@ const ProductPage = async ({ params }: ProductVariantPageProps) => {
         <p>Nenhum produto encontrado</p>
       </>
     );
-=======
-    },
-  });
-
-  if (!likelyProducts) return (<>
-  <p>Nenhum produto encontrado</p>
-  </>);
->>>>>>> c7183bf43307b805b7149197807688e64cd22dc5
 
   return (
     <>
@@ -73,7 +57,6 @@ const ProductPage = async ({ params }: ProductVariantPageProps) => {
           sizes="100vw"
           height={0}
           width={0}
-<<<<<<< HEAD
           className="h-auto w-full rounded-3xl p-4"
         />
       </div>
@@ -81,14 +64,6 @@ const ProductPage = async ({ params }: ProductVariantPageProps) => {
         <div className="px-6">
           <VariantSelector selectedVariantSlug={productVariant.slug} variants={productVariant.product.variants} />
         </div>
-=======
-          className="h-auto w-full rounded-3xl"
-        />
-      </div>
-      <div className="flex flex-col space-y-6">
-        <div className="px-6">{/* Variantes */}</div>
-
->>>>>>> c7183bf43307b805b7149197807688e64cd22dc5
         <div className="px-6">
           <h2 className="text-lg font-semibold">
             {productVariant.product.name}
@@ -118,11 +93,7 @@ const ProductPage = async ({ params }: ProductVariantPageProps) => {
           </p>
         </div>
         <div className="space-y-6">
-<<<<<<< HEAD
           <ProductsList title="Mais vendidos" products={likelyProducts} />
-=======
-            <ProductsList title="Mais vendidos" products={likelyProducts}/>
->>>>>>> c7183bf43307b805b7149197807688e64cd22dc5
         </div>
       </div>
 
