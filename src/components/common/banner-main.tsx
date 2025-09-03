de0b6028d5192b6a250fc3085bb2bf55c0e9d459
Diego Carlos <diegoc.gayoso@gmail.com>
2025-09-03 12:21:25 -0300

    fix: remove carousel shadcn to banner-main

--- src/components/common/banner-main.tsx
+++ src/components/common/banner-main.tsx
@@ -1,14 +1,7 @@
 "use client";
 import Image from "next/image";
 import { Anton } from "next/font/google";
-import Autoplay from "embla-carousel-autoplay";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+
 import React from "react";
 
 const anton = Anton({
@@ -17,50 +10,41 @@ const anton = Anton({
 });
 
 const BannerMain = () => {
-  const plugin = React.useRef(
-    Autoplay({ delay: 5000, stopOnInteraction: true })
-  );
+  const [index, setIndex] = React.useState(0);
 
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex < 2 ? prevIndex + 1 : 0));
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="container mx-auto space-y-6 p-5">
-      <div className="relative m-auto h-[75vh] w-full rounded-2xl bg-[url(/banner-main-bg-mobile.png)] bg-contain bg-no-repeat py-24 md:bg-[url(/banner-main-bg.png)] md:bg-cover md:bg-no-repeat">
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          plugins={[plugin.current]}
-          className="w-full"
-          onMouseEnter={plugin.current.stop}
-          onMouseLeave={plugin.current.reset}
-        >
-          <CarouselContent>
-            {Array.from({ length: 4 }).map((_, index) => (
-              <CarouselItem key={index} className="flex items-center justify-center">
-                <div className="relative h-[57vh] w-full">
-                  <Image
-                    src={`/man-banner0${index + 1}.svg`}
-                    alt={`Banner Main ${index + 1}`}
-                    fill
-                    style={{ objectFit: "contain" }}
-                    className="z-10"
-                  />
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious className="left-2 z-20" />
-          <CarouselNext className="right-2 z-20" />
-        </Carousel>
+      <div className="relative h-auto w-full">
+        <Image
+          src="/banner-main-bg-mobile.png"
+          width={365}
+          height={460}
+          alt="Banner Leve a vida com estilo"
+          className="mx-auto md:hidden"
+        />
+        <Image
+          src="/banner-main-bg.png"
+          alt="Bewear"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="hidden h-auto w-full md:block"
+        />
 
-        {/* <div className="mx-auto mt-4 w-3/4 text-center md:hidden">
-          <p className="font-thin text-white uppercase">
-            Leve uma <br />
-            <span className={`${anton.className} text-6xl font-bold`}>
-              vida com estilo
-            </span>
-          </p>
-        </div> */}
+        <Image
+          src={`/man-banner0${index + 1}.svg`}
+          alt={`Banner Main ${index + 1}`}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="absolute bottom-0 z-10 h-[75vh] md:h-auto w-full"
+        />
       </div>
     </div>
   );
