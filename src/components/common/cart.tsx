a0e4c559234aefdf867bc2848200fa9c1315df9c
Diego Carlos <diegoc.gayoso@gmail.com>
2025-09-04 13:00:38 -0300

    feat: add cart component and ensure a product can be add to cart

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,12 +1,25 @@
 "use client";
+
 import {
   Sheet,
   SheetContent,
+  SheetHeader,
+  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "../ui/button";
 import { ShoppingBasketIcon } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { getCart } from "@/actions/get-cart";
+
+import formatCentsToBrl from "@/helpers/money";
+
 const Cart = () => {
+  const { data: cart, isPending: cartIsPending } = useQuery({
+    queryKey: ["cart"],
+    queryFn: () => getCart(),
+  });
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -14,7 +27,35 @@ const Cart = () => {
           <ShoppingBasketIcon />
         </Button>
       </SheetTrigger>
-      <SheetContent></SheetContent>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Seu carrinho</SheetTitle>
+        </SheetHeader>
+        {cartIsPending && <p>Carregando...</p>}
+        {cart?.items.length === 0 && (
+          <div className="px-4 mx-auto text-center">
+            <p>Seu carrinho está vazio</p>
+          </div>
+        )}
+        {cart?.items.map((item) => (
+          <div key={item.id} className="mb-4 flex items-center space-x-4">
+            <img
+              src={item.productVariant.imageUrl}
+              alt={item.productVariant.name}
+              className="h-16 w-16 rounded object-cover"
+            />
+            <div>
+              <h3 className="text-lg font-medium">
+                {item.productVariant.name}
+              </h3>
+              <p className="text-sm text-gray-500">
+                {item.quantity} x{" "}
+                {formatCentsToBrl(item.productVariant.priceInCents)}
+              </p>
+            </div>
+          </div>
+        ))}
+      </SheetContent>
     </Sheet>
   );
 };
